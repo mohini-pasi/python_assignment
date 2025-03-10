5a9997eccf6a009b4c98a3686c267cc82ea51982
mohini-pasi <mohinipasi1122@gmail.com>
2025-03-10 11:20:32 +0530

    Update arbitrary_argument.py

--- Python-arguments-types/arbitrary_argument.py
+++ Python-arguments-types/arbitrary_argument.py
@@ -1,3 +1,4 @@
+```python
 def arbitrary_agrument(*args):
     sum = 0
     for i in args:
@@ -5,3 +6,4 @@ def arbitrary_agrument(*args):
     return sum
 data=arbitrary_agrument(12,23,34,45,45)
 print(data)          #output 159
+```
