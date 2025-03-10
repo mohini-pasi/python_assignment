4c26649e38a6c464e47b89a1714f43dcfd54c182
mohini-pasi <mohinipasi1122@gmail.com>
2025-03-10 11:15:53 +0530

    Update chaining_decorator.py

--- function/chaining_decorator.py
+++ function/chaining_decorator.py
@@ -1,3 +1,30 @@
+Example 1: 
+
+For num() function we are applying 2 decorator functions. Firstly the inner decorator will work and then the outer decorator.
+
+
+# code for testing decorator chaining
+def decor1(func):
+    def inner():
+        x = func()
+        return x * x
+    return inner
+
+def decor(func):
+    def inner():
+        x = func()
+        return 2 * x
+    return inner
+
+@decor1
+@decor
+def num():
+    return 10
+
+print(num())
+Output:
+
+400
 
 
      
